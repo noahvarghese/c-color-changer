5f3409e994ab86b918cd87801afd1202aa499216
Noah Varghese <varghese.noah@gmail.com>
2021-06-05 10:27:19 -0400

    more parsing files

--- src/main.c
+++ src/main.c
@@ -2,53 +2,76 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <dirent.h>
 
 #include "./util/args.h"
 #include "./util/vars.h"
 #include "./util/checks.h"
 #include "./util/color_changer.h"
 
-void perform_on_files(char *files[], char prev_path[], void (*func)(char[])) {
-    int size = sizeof files / sizeof files[0];
+char **get_files(char dir[])
+{
+    char *files[];
 
-    for ( int i = 0; i < size; i++ ) {
-        char *file;
+    return files;
+}
+
+void perform_on_files(int files_count, char *files[], char prev_path[], void (*func)(char[]))
+{
+    int size = files_count / sizeof files[0];
+
+    for (int i = 0; i < size; i++)
+    {
+        char file[5];
         strcpy(file, files[i]);
 
         // if prev_path is empty
         // then set prev_path to files[i]
         // else
-        if ( prev_path == "" ) {
+        if (prev_path == "")
+        {
             strcpy(prev_path, "./");
             strcpy(prev_path, file);
         }
-        else {
-            if (prev_path[(int)strlen(prev_path) - 1] == "/") {
+        else
+        {
+            if (prev_path[(int)strlen(prev_path) - 1] == "/")
+            {
                 strcpy(prev_path, file);
             }
-            else {
+            else
+            {
                 strcpy(prev_path, "/");
                 strcpy(prev_path, file);
             }
         }
 
+        printf("prev_path: %s", prev_path);
+
         // if isdir()
-        if ( check_is_dir(file) == true ) {
-            go_through_files(file, prev_path, func);
+        if (check_is_dir(file) == true)
+        {
+
+            perform_on_files()
+                go_through_files(1, file, prev_path, func);
         }
-        else {
+        else
+        {
             // perform change
             func(file);
         }
     }
 }
 
-int close_program(bool success) {
+int close_program(bool success)
+{
     free(PATH);
     free(IGNORED_COLOR);
     free(DESIRED_COLOR);
     return success;
 }
+
+void tmp_func(char yolo[]) {}
 
 int main(int argc, char *argv[])
 {
@@ -58,13 +81,14 @@ int main(int argc, char *argv[])
     // -c/--color desired color
     // -p/--path image path (directory or file)
 
-    if ( parse_args(argc, argv) == false ) {
+    if (parse_args(argc, argv) == false)
+    {
         printf(HELP_MESSAGE);
         return close_program(success);
     }
-    
+
     char *file[] = {{PATH}};
-    perform_on_files(file, "", color_changer);
+    perform_on_files(1, file, "", tmp_func);
 
     // For each file
 
