145f449b7a8d7541c8ce003c3d0d3a9f4a807982
noahvarghese <varghese.noah@gmail.com>
2021-06-09 22:03:44 +0000

    added checks and sample images

--- src/util/checks.c
+++ src/util/checks.c
@@ -61,3 +61,46 @@ bool check_color_format(char *color)
 
     return true;
 }
+
+bool check_is_png(FILE *file) {
+    if ( file == NULL ) {
+        return false;
+    }
+
+    unsigned char bytes[8];
+    fread(bytes, 8, 1, file);
+
+    if (
+        bytes[0] == 137 &&
+        bytes[1] == 80 &&
+        bytes[2] == 78 &&
+        bytes[3] == 71 &&
+        bytes[4] == 13 &&
+        bytes[5] == 10 &&
+        bytes[6] == 26 &&
+        bytes[7] == 10
+    ) {
+        return true;
+    }
+
+    return false;
+}
+
+bool check_is_jpg(FILE *file) {
+    if ( file == NULL ) {
+        return false;
+    }
+
+    unsigned char bytes[3];
+    fread(bytes, 3, 1, file);
+
+    if (
+        bytes[0] == 0xff &&
+        bytes[1] == 0xd8 &&
+        bytes[2] == 0xff
+    ) {
+        return true;
+    }
+
+    return false;
+}
