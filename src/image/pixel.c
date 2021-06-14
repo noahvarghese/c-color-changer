b0178fc9ce74a929c5d928169064cdf3a6978d76
noahvarghese <varghese.noah@gmail.com>
2021-06-14 21:43:15 +0000

    poor choices

--- src/image/pixel.c
+++ src/image/pixel.c
@@ -131,6 +131,8 @@ int *png_bytep_to_hsb(png_bytep px)
 
 int *intp_to_hsb(int *px)
 {
+    printf("RGBA -> r: %3d g: %3d b: %3d a: %3d\n", px[0], px[1], px[2], px[3]);
+
     int *hsl = (int *)malloc(sizeof(int) * 4);
 
     // make ranger from 0 - 1
@@ -168,6 +170,7 @@ int *intp_to_hsb(int *px)
     hsl[2] = (int)round(l);
     hsl[3] = a;
 
+    printf("HSLA -> h: %3d s: %3d l: %3d a: %3d\n", hsl[0], hsl[1], hsl[2], hsl[3]);
     return hsl;
 }
 
@@ -175,6 +178,131 @@ png_bytep hsb_to_png_bytep(int *hsb)
 {
 }
 
+// THIS IS HSL NOT HSV
 int *hsb_to_intp(int *hsb)
 {
+    printf("HSBA -> h: %3d s: %3d b: %3d a: %3d\n", hsb[0], hsb[1], hsb[2], hsb[3]);
+    int r, g, b;
+    float _r, _g, _b;
+
+    float h = (float)hsb[0],
+          s = (float)hsb[1] / 100.0,
+          l = ((float)hsb[2]) / 100.0,
+          a = (float)hsb[3];
+
+    printf("HSBA -> h: %3f s: %3f b: %3f a: %3f\n", h, s, l, hsb[3]);
+
+    float c = round((1.0 - abs(2.0 * l - 1.0)) * s);
+    float x = round(c * (1.0 - abs(fmodf((h / 60.0), 2.0)) - 1.0));
+    float m = (l - (c / 2.0));
+
+    if (h < 0.0 || h > 360.0)
+        h = 0.0;
+
+    if (h < 60.0)
+    {
+        printf("HERE\n");
+        _r = c;
+        _g = x;
+        _b = 0.0;
+    }
+
+    else if (h < 120.0)
+    {
+        _r = x;
+        _g = c;
+        _b = 0.0;
+    }
+
+    else if (h < 180.0)
+    {
+        _r = 0.0;
+        _g = c;
+        _b = x;
+    }
+
+    else if (h < 240.0)
+    {
+        _r = 0.0;
+        _g = x;
+        _b = c;
+    }
+
+    else if (h < 300.0)
+    {
+        _r = x;
+        _g = 0.0;
+        _b = c;
+    }
+
+    else if (h < 360.0)
+    {
+        _r = c;
+        _g = 0.0;
+        _b = x;
+    }
+
+    r = (int)((_r + m) * 255.0);
+    g = (int)((_g + m) * 255.0);
+    b = (int)((_b + m) * 255.0);
+
+    // double hh, p, q, t, ff;
+    // long i;
+
+    // hh = hsb[0];
+    // if (hh >= 360.0)
+    //     hh = 0.0;
+
+    // hh /= 60.0;
+
+    // i = (long)hh;
+    // ff = hh - i;
+    // p = hsb[2] * (1.0 - hsb[1]);
+    // q = hsb[2] * (1.0 - (hsb[1] * ff));
+    // t = hsb[2] * (1.0 - (hsb[2] * (1.0 - ff)));
+
+    // printf("I: %d\n", i);
+    // switch (i)
+    // {
+    // case 0:
+    //     r = hsb[2];
+    //     g = t;
+    //     b = p;
+    //     break;
+    // case 1:
+    //     r = q;
+    //     g = hsb[2];
+    //     b = p;
+    //     break;
+    // case 2:
+    //     r = p;
+    //     g = hsb[2];
+    //     b = t;
+    //     break;
+
+    // case 3:
+    //     r = p;
+    //     g = q;
+    //     b = hsb[2];
+    //     break;
+    // case 4:
+    //     r = t;
+    //     g = p;
+    //     b = hsb[2];
+    //     break;
+    // case 5:
+    // default:
+    //     r = hsb[2];
+    //     g = p;
+    //     b = q;
+    //     break;
+    // }
+
+    int *rgba = (int *)malloc(sizeof(int) * 4);
+    rgba[0] = r;
+    rgba[1] = g;
+    rgba[2] = b;
+    rgba[3] = a;
+    printf("RGBA -> r: %3d g: %3d b: %3d a: %3d\n\n", rgba[0], rgba[1], rgba[2], rgba[3]);
+    return rgba;
 }
