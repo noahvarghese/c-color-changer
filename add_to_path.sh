1d4863f467e52a0e16e365d086e3fbb241f29010
Noah Varghese <varghese.noah@gmail.com>
2021-06-30 03:00:29 +0000

    Create add_to_path.sh

---
+++ add_to_path.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo mv ./png-color-changer /usr/local/bin
+sudo chmod 744 /usr/local/bin/png-color-changer
+sudo chgrp users /usr/local/bin/png-color-changer
+sudo chown user /usr/local/bin/png-color-changer
